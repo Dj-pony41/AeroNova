818967353ffdf5502d49e23049661ca9f3d6f4ab
Dj-pony41 <vqj2022952@est.univalle.edu>
2025-04-07 22:12:04 -0400

    up

--- sync-agent/src/sync/sync.module.ts
+++ sync-agent/src/sync/sync.module.ts
@@ -1,23 +1,25 @@
 import { Module } from '@nestjs/common';
 import { SyncGateway } from './sync/sync.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { SyncMysqlService } from './sync/sync-mysql.service';
 import { SyncMongoService } from './sync/sync-mongo.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Asiento } from '../../src/mysql/asiento/entities/asiento.entity';
-import { MongooseModule } from '@nestjs/mongoose';
-import { MongoAsiento, AsientoSchema } from '../../src/mongo/asiento/entities/asiento.entity';
 
-const dbType = process.env.DB_TYPE;
+import { Asiento as MySQLAsiento } from '../mysql/asiento/entities/asiento.entity';
+import { MongoAsiento, AsientoSchema } from '../mongo/asiento/entities/asiento.entity';
+
+const isMySQL = process.env.DB_TYPE === 'mysql';
 
 @Module({
   imports: [
-    ...(dbType === 'mysql'
-      ? [TypeOrmModule.forFeature([Asiento])]
+    ...(isMySQL
+      ? [TypeOrmModule.forFeature([MySQLAsiento])]
       : [MongooseModule.forFeature([{ name: MongoAsiento.name, schema: AsientoSchema }])]),
   ],
   providers: [
     SyncGateway,
-    ...(dbType === 'mysql' ? [SyncMysqlService] : [SyncMongoService]),
+    ...(isMySQL ? [SyncMysqlService] : [SyncMongoService]),
   ],
 })
 export class SyncModule {}
