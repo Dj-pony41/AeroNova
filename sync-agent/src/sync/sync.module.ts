9f66246de4ef2839f625441a484a7a60224ac839
Dj-pony41 <163031918+Dj-pony41@users.noreply.github.com>
2025-04-07 23:19:49 -0400

    up

--- sync-agent/src/sync/sync.module.ts
+++ sync-agent/src/sync/sync.module.ts
@@ -2,24 +2,35 @@ import { Module } from '@nestjs/common';
 import { SyncGateway } from './sync/sync.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MongooseModule } from '@nestjs/mongoose';
-
 import { SyncMysqlService } from './sync/sync-mysql.service';
-import { SyncMongoService } from './sync/sync-mongo.service';
 
 import { Asiento as MySQLAsiento } from '../mysql/asiento/entities/asiento.entity';
-import { MongoAsiento, AsientoSchema } from '../mongo/asiento/entities/asiento.entity';
 
 const isMySQL = process.env.DB_TYPE === 'mysql';
 
+const imports = isMySQL
+  ? [TypeOrmModule.forFeature([MySQLAsiento])]
+  : [
+      // ¡Este bloque se carga dinámicamente solo si Mongo!
+      (() => {
+        const { MongoAsiento, AsientoSchema } = require('../mongo/asiento/entities/asiento.entity');
+        return MongooseModule.forFeature([{ name: MongoAsiento.name, schema: AsientoSchema }]);
+      })(),
+    ];
+
+const providers = isMySQL
+  ? [SyncGateway, SyncMysqlService]
+  : [
+      SyncGateway,
+      // Se carga dinámicamente solo si Mongo
+      (() => {
+        const { SyncMongoService } = require('./sync-mongo.service');
+        return SyncMongoService;
+      })(),
+    ];
+
 @Module({
-  imports: [
-    ...(isMySQL
-      ? [TypeOrmModule.forFeature([MySQLAsiento])]
-      : [MongooseModule.forFeature([{ name: MongoAsiento.name, schema: AsientoSchema }])]),
-  ],
-  providers: [
-    SyncGateway,
-    ...(isMySQL ? [SyncMysqlService] : [SyncMongoService]),
-  ],
+  imports,
+  providers,
 })
 export class SyncModule {}
