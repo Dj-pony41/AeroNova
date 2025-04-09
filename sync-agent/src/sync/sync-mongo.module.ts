c44316646cf96c39ecfc13ee2941be5e2b011f25
Dj-pony41 <163031918+Dj-pony41@users.noreply.github.com>
2025-04-09 10:39:54 -0400

    up

--- sync-agent/src/sync/sync-mongo.module.ts
+++ sync-agent/src/sync/sync-mongo.module.ts
@@ -4,9 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SyncGateway } from './sync/sync.gateway';
 import { SyncMongoService } from './sync/sync-mongo.service';
 import { MongoAsiento, AsientoSchema } from '../mongo/asiento/entities/asiento.entity';
+import { AsientoModule } from '../mongo/asiento/asiento.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: MongoAsiento.name, schema: AsientoSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: MongoAsiento.name, schema: AsientoSchema }]),
+    AsientoModule, // ✅ AÑADE ESTO
+  ],
   providers: [SyncGateway, SyncMongoService],
   exports: [SyncGateway, SyncMongoService],
 })
