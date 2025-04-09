cca74893661b64be268c7729628027b791a98c7c
Dj-pony41 <163031918+Dj-pony41@users.noreply.github.com>
2025-04-09 12:00:08 -0400

    up

--- sync-agent/src/sync/sync-mongo.module.ts
+++ sync-agent/src/sync/sync-mongo.module.ts
@@ -5,11 +5,13 @@ import { SyncGateway } from './sync/sync.gateway';
 import { SyncMongoService } from './sync/sync-mongo.service';
 import { MongoAsiento, AsientoSchema } from '../mongo/asiento/entities/asiento.entity';
 import { AsientoModule } from '../mongo/asiento/asiento.module';
+import { PasajeroModule } from '../mongo/pasajero/pasajero.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: MongoAsiento.name, schema: AsientoSchema }]),
-    AsientoModule, // ✅ AÑADE ESTO
+    AsientoModule,
+        PasajeroModule, // ✅ AÑADE ESTO
   ],
   providers: [SyncGateway, SyncMongoService],
   exports: [SyncGateway, SyncMongoService],
--- sync-agent/src/sync/sync-mysql.module.ts
+++ sync-agent/src/sync/sync-mysql.module.ts
@@ -5,11 +5,14 @@ import { SyncMysqlService } from './sync/sync-mysql.service';
 import { Asiento as MySQLAsiento } from '../mysql/asiento/entities/asiento.entity';
 import { WebSocketClient } from './websocket.client';
 import { AsientoModule } from '../mysql/asiento/asiento.module';
+import { PasajeroModule } from '../mysql/pasajero/pasajero.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([MySQLAsiento]),
-    forwardRef(() => AsientoModule), // ✅ Rompe el ciclo
+    forwardRef(() => AsientoModule),
+    
+    PasajeroModule, // ✅ Rompe el ciclo
   ],
   providers: [SyncGateway, SyncMysqlService, WebSocketClient],
   exports: [SyncGateway, SyncMysqlService, WebSocketClient],
